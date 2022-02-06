0efd27cae0d30c604b056faae38c47b4bf2da103
lordavadon <lordavadon22@gmail.com>
2022-02-06 12:00:35 +0200

    DB config
    - add alembic
    - create folders of project
    - create model User
    - add config to alembic

---
+++ ecommerce/config.py
@@ -0,0 +1,8 @@
+import os
+
+APP_ENV = os.getenv('APP_ENV', '*******')
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', '*******')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', '*******')
+DATABASE_HOST = os.getenv('DATABASE_HOST', '*******')
+DATABASE_PORT = os.getenv('DATABASE_PORT', '*******')
+DATABASE_NAME = os.getenv('DATABASE_NAME', '*******')
