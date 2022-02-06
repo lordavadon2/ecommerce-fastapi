8f43f347baddfcab263a7b111b481aab9dafcc4e
lordavadon <lordavadon22@gmail.com>
2022-02-06 20:44:42 +0200

    CRUD
    - add get_user, get_list_users, delete_user

--- ecommerce/user/router.py
+++ ecommerce/user/router.py
@@ -1,13 +1,18 @@
+from __future__ import annotations
+
+from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from starlette.responses import Response
 
 from ecommerce import db
 from ecommerce.user import schema, validator, services, models
 
-router = APIRouter(tags=['User'], prefix='/user')
+router = APIRouter(tags=['User'], prefix='/users')
 
 
-@router.post('/registration',
+@router.post('/create',
              status_code=status.HTTP_201_CREATED)
 async def create_user_registration(request: schema.User,
                                    database: Session = Depends(db.get_db)) -> models.User:
@@ -19,3 +24,18 @@ async def create_user_registration(request: schema.User,
         )
     new_user = await services.create_user(request, database)
     return new_user
+
+
+@router.get('/', response_model=List[schema.DisplayUser])
+async def get_all_users(database: Session = Depends(db.get_db)) -> List[models.User]:
+    return await services.get_all_users(database)
+
+
+@router.get('/{user_id}', response_model=schema.DisplayUser)
+async def get_all_users(user_id: int, database: Session = Depends(db.get_db)) -> models.User:
+    return await services.get_user_by_id(user_id, database)
+
+
+@router.delete('/{user_id}', status_code=status.HTTP_204_NO_CONTENT, response_class=Response)
+async def delete_user_by_id(user_id: int, database: Session = Depends(db.get_db)):
+    return await services.delete_user_by_id(user_id, database)
--- ecommerce/user/services.py
+++ ecommerce/user/services.py
@@ -1,3 +1,8 @@
+from __future__ import annotations
+
+from typing import List
+
+from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 
 from ecommerce.user import models, schema
@@ -9,3 +14,19 @@ async def create_user(request: schema.User, db_session: Session) -> models.User:
     db_session.commit()
     db_session.refresh(new_user)
     return new_user
+
+
+async def get_all_users(db_session: Session) -> List[models.User]:
+    return db_session.query(models.User).all()
+
+
+async def get_user_by_id(user_id: int, db_session: Session) -> models.User:
+    user_info = db_session.query(models.User).get(user_id)
+    if not user_info:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User Not Found!")
+    return user_info
+
+
+async def delete_user_by_id(user_id: int, db_session: Session):
+    db_session.query(models.User).filter(models.User.id == user_id).delete()
+    db_session.commit()
